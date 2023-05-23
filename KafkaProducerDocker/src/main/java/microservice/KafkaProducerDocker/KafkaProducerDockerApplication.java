0a7f5f5e8f44b580d1323ffb1a2f9b661a32b308
Lyubomyr <bobboby403@gmail.com>
2023-05-24 00:30:23 +0300

    start project Kafka.Docker.Mailing service ver 1.0

---
+++ KafkaProducerDocker/src/main/java/microservice/KafkaProducerDocker/KafkaProducerDockerApplication.java
@@ -0,0 +1,13 @@
+package microservice.KafkaProducerDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerDockerApplication.class, args);
+	}
+
+}
