5470a3dbbaed4312517c5ac76376acc2b7e78348
Lyubomyr <bobboby403@gmail.com>
2023-05-24 14:03:49 +0300

    created service KafkaConsumer

---
+++ KafkaConsumerDocker/src/main/java/microservice/KafkaConsumerDocker/KafkaConsumerDockerApplication.java
@@ -0,0 +1,13 @@
+package microservice.KafkaConsumerDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerDockerApplication.class, args);
+	}
+
+}
---
+++ KafkaConsumerDocker/src/test/java/microservice/KafkaConsumerDocker/KafkaConsumerDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package microservice.KafkaConsumerDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
